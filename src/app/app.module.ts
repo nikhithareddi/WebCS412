97e46c659baf9d914dbf4ade3226baa1355017be
Sri Nikhitha <srinikhitha2605@gmail.com>
2024-11-16 22:47:58 -0600

    WEB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,35 @@ import { FooterComponent } from './navigation/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
+import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatCardModule} from "@angular/material/card";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreatePostComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
